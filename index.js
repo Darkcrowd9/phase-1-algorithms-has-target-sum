4429a7b1c2e73578576eef30e347183b9f94972a
Ian <cheruiyotian84@gmail.com>
2024-07-24 19:02:36 +0300

    Completed task js & json files

--- index.js
+++ index.js
@@ -29,11 +29,6 @@ function hasTargetSum(array, target) {
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
-
-  console.log("");
-
-  console.log("Expecting: true");
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
   console.log("");
@@ -41,5 +36,11 @@ if (require.main === module) {
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+
+  console.log("");
+
+  
 
 module.exports = hasTargetSum;
